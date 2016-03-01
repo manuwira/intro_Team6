3e87c84364e7d69486df5e9a2542c73f35197023
Manuel Wittmer <ma_wittmer@hotmail.com>
2016-03-01 08:43:59 +0100

    new
    haba.h

---
+++ projects/INTRO_Common/Common/haba.h
@@ -0,0 +1,15 @@
+/*
+ * haba.h
+ *
+ *  Created on: 01.03.2016
+ *      Author: manuelwittmer
+ */
+
+#ifndef SOURCES_COMMON_HABA_H_
+#define SOURCES_COMMON_HABA_H_
+
+
+
+
+
+#endif /* SOURCES_COMMON_HABA_H_ */
